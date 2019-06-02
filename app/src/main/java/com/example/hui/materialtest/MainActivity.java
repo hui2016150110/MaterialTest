c9c2f50b7da5502819776e38913e4b388cef10f8
Huang Chaohui <1466825121@qq.com>
2019-06-02 14:21:10 +0800

    "添加DrawerLayout"

--- app/src/main/java/com/example/hui/materialtest/MainActivity.java
+++ app/src/main/java/com/example/hui/materialtest/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.hui.materialtest;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -8,12 +11,22 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        ActionBar actionBar = getSupportActionBar();
+
+        if(actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.navigation);
+
+        }
+
     }
 
     @Override
@@ -30,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
             case R.id.delete:
                 break;
             case R.id.settings:
+                break;
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
                 break;
             default:
                 break;
